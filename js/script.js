e0edf1fa2bbd64c77bd08cb84206ad14fa73d030
Junona N <j.vyushkova@gmail.com>
2014-06-15 23:07:42 +0400

    update files

---
+++ js/script.js
@@ -0,0 +1,31 @@
+window.onload=function (){
+
+	$.fn.cycle.defaults.autoSelector = '.slides';
+
+    var popupOpen    = $(".popup-toggle"),
+        popupOverlay = $(".popup-overlay");
+        popup        = $(".popup");
+ 
+    popupOpen.on("click", function (e) {
+        e.preventDefault();
+ 		
+ 		popupOverlay.css("display","block").addClass("fadeIn");
+        popup.addClass("fadeInDown");
+        setTimeout(function () {
+				popupOverlay.removeClass("fadeOut").css("opacity","1");
+        		popup.removeClass("fadeOut").css("opacity","1");
+            }, 1000);
+    });
+ 
+    popupOverlay.on("click", function (e) {
+        if (e.currentTarget === e.target) {
+            popup.addClass("fadeOut");
+            popupOverlay.addClass("fadeOut");
+            setTimeout(function () {
+				popup.removeClass("fadeInDown").css("opacity","0").removeClass("fadeOut");
+				popupOverlay.removeClass("fadeIn").removeClass("fadeOut").css("opacity","0").css("display","none");
+            }, 1000);
+        }
+    });
+ 
+};
